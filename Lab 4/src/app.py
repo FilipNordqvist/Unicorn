a1afe4150e85f481616f7198c9577f35ef7f0ed2
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-03-14 13:31:34 +0100

    Börjar med att läsa mer dokumentation

--- Lab 4/src/app.py
+++ Lab 4/src/app.py
@@ -2,8 +2,14 @@
 
 from fastapi import FastAPI
 
+import httpx
+
 app = FastAPI()
 
 @app.get("/")
 async def hello():
     return {"Hello": "World!"}
+
+@app.get("/{id}")
+async def get_unicorn(id: str):
+   return False
